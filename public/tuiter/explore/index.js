45fcbe80978d21b84c3b07274fc0b313088761ef
Samantha Olinsky <67108089+samantha-olinsky@users.noreply.github.com>
2023-02-18 15:37:46 -0500

    fixed direct js link

--- public/tuiter/explore/index.js
+++ public/tuiter/explore/index.js
@@ -1,6 +1,6 @@
 /* eslint-env jquery */
 import NavigationSidebar from "../NavigationSidebar/index.js";
-import WhoToFollowList from "../WhoToFollowList";
+import WhoToFollowList from "../WhoToFollowList/index.js";
 import MainContent from "./MainContent.js";
 
 function exploreComponent() {
