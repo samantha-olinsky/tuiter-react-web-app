98ce8367fc49612a488228e24852e8765b43a9ce
Samantha Olinsky <67108089+samantha-olinsky@users.noreply.github.com>
2023-02-24 16:56:18 -0500

    finished a6

--- src/tuiter/home/PostItem.js
+++ src/tuiter/home/PostItem.js
@@ -22,9 +22,26 @@ const PostItem = (
       <li className="list-group-item d-flex flex-row w-100">
           <img className="rounded-5 me-3 avatar-img mt-1" src={`/images/${post.avatarIcon}`}/>
           <div className="flex-column flex-fill">
-              <p><span className="fw-bold">{post.authorName} </span><i className="fa fa-check-circle"></i> <span className="text-secondary">@{post.handle} • {post.time}</span></p>
+              <p className="mb-0"><span className="fw-bold">{post.authorName} </span><i className="fa fa-check-circle"></i> <span className="text-secondary">@{post.handle} • {post.time}</span></p>
               <p>{post.text}</p>
               {post.hasOwnProperty("image") ? PostItemImage(post.image) : PostItemCard(post.card)}
+              <div className="d-flex flex-row w-100 pt-3 pb-2">
+                <span className="flex-fill wd-fg-color-gray">
+                  <i className="fa-regular fa-comment"></i>
+                  <p className="d-inline ps-3">{post.numComments}</p>
+                </span>
+                <span  className="flex-fill wd-fg-color-gray">
+                  <i className="fa-solid fa-retweet"></i>
+                  <p className="d-inline ps-3">{post.numRetuits}</p>
+                </span>
+                <span className="flex-fill wd-fg-color-gray">
+                  <i className="fa-solid fa-heart"></i>
+                  <p className="d-inline ps-3">{post.numLikes}</p>
+                </span>
+                <span className="flex-fill wd-fg-color-gray">
+                  <i className="fa-solid fa-arrow-up-from-bracket"></i>
+                </span>
+              </div>
           </div>
       </li>
   );
--- src/tuiter/home/PostItemCard.js
+++ src/tuiter/home/PostItemCard.js
@@ -3,10 +3,10 @@ const PostItemCard = (card) => {
   return(
       <div className="card">
         <img className="card-img-top" src={`/images/${card.image}`}/>
-        <div className="card-body bg-body">
+        <div className="card-body">
           <h6 className="card-title">{card.imageTitle}</h6>
-          <p className="card-text">{card.imageCaption}</p>
-          <p className="card-text"><i className="fa fa-link"></i>{card.imageLink}
+          <p className="card-text wd-fg-color-gray">{card.imageCaption}</p>
+          <p className="card-text wd-fg-color-gray"><i className="fa fa-link"></i>{card.imageLink}
           </p>
         </div>
       </div>
