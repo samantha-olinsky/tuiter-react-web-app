d0c14c5ed1aac26b3315a65a36793ab99fef1d8d
Samantha Olinsky <67108089+samantha-olinsky@users.noreply.github.com>
2023-03-24 12:37:45 -0400

    refactor nav items

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import {Link} from "react-router-dom";
 import {useLocation} from "react-router";
 import "./index.css";
+import NavItem from "./NavItem";
+import navItems from "./nav-items";
 
 const NavigationSidebar = () => {
   const {pathname} = useLocation();
@@ -10,51 +11,7 @@ const NavigationSidebar = () => {
   return (
       <div className="list-group">
         <span className="list-group-item">Tuiter</span>
-        <Link to="/tuiter/" className={`list-group-item
-                    ${active === ""?'active':''}`}>
-          <span className="wd-nav-icon"><i className="fa fa-home"/></span>
-          <span className="d-xl-inline d-none ">Home</span>
-        </Link>
-        <Link to="/tuiter/explore" className={`list-group-item
-                    ${active === 'explore'?'active':''}`}>
-          <span className="wd-nav-icon"><i className="fa fa-hashtag"/></span>
-          <span className="d-xl-inline d-none ">Explore</span>
-        </Link>
-        <Link to="/" className={`list-group-item
-                    ${active === 'more'?'active':''}`}>
-          <span className="wd-nav-icon"><i className="fa fa-asterisk"/></span>
-          <span className="d-xl-inline d-none ">Labs</span>
-        </Link>
-        <Link to="#" className={`list-group-item
-                    ${active === 'notifications'?'active':''}`}>
-          <span className="wd-nav-icon"><i className="fa fa-bell"/></span>
-          <span className="d-xl-inline d-none ">Notifications</span>
-        </Link>
-        <Link to="#" className={`list-group-item
-                    ${active === 'messages'?'active':''}`}>
-          <span className="wd-nav-icon"><i className="fa fa-envelope"/></span>
-          <span className="d-xl-inline d-none ">Messages</span>
-        </Link>
-        <Link to="#" className={`list-group-item
-                    ${active === 'bookmarks'?'active':''}`}>
-          <span className="wd-nav-icon"><i className="fa fa-bookmark"/></span>
-          <span className="d-xl-inline d-none ">Bookmarks</span>
-        </Link>
-        <Link to="#" className={`list-group-item
-                    ${active === 'lists'?'active':''}`}>
-          <span className="wd-nav-icon"><i className="fa fa-list"/></span>
-          <span className="d-xl-inline d-none ">Lists</span>
-        </Link>
-        <Link to="/tuiter/profile" className={`list-group-item
-                    ${active === 'profile'?'active':''}`}>
-          <span className="wd-nav-icon"><i className="fa fa-user"/></span>
-          <span className="d-xl-inline d-none ">Profile</span>
-        </Link>
-        <Link to="#" className={`list-group-item
-                    ${active === 'more'?'active':''}`}>
-          <span className="wd-nav-icon"><i className="fa fa-ellipsis-h"/></span>
-          <span className="d-xl-inline d-none ">More</span>
-        </Link>
+        {navItems.map(navItem => <NavItem navItem={navItem} active={active}/>)}
       </div>
   );
 };
